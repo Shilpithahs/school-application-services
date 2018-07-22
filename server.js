002562b06e9d39d75467e1cdf901a660ef8b7a12
Shilpitha HS <shilpitha.rb171@in.ad.dom>
2018-07-22 12:26:40 +0530

    Updated Changes

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ MongoClient.connect(DATABASE_URL, { useNewUrlParser: true }, (err, database) => 
 })
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
+    res.header("Content-Type", "application/json");
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -25,9 +27,8 @@ app.use(function(req, res, next) {
     next();
 });
 
+// Add user service
 app.post('/user', (req, res) => {
-    console.log('user req---------', req);
-    console.log('---------------------------');
     db.collection('users').save(req.body, (err, result) => {
     if(err) {
         console.log(err);
@@ -37,6 +38,7 @@ app.post('/user', (req, res) => {
     })
 })
 
+// Get user service
 app.get('/user', (req, res) => {
     db.collection('users').find().toArray((err, result) => {
     if (err) return console.log(err)
@@ -45,22 +47,49 @@ app.get('/user', (req, res) => {
     })
 })
 
-app.post('/studentList', (req, res) => {
-    console.log('req---------', req);
-    console.log('---------------------------');
+// Add Student service
+app.post('/addSutudent', (req, res) => {
     db.collection('studentList').save(req.body, (err, result) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('saved to database');
-    }
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('saved to database');
+        }
     })
 })
 
-app.get('/getAll', (req, res) => {
+// Get all Student service
+app.get('/getAllStudents', (req, res) => {
     db.collection('studentList').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    console.log(result);
-    res.json(result);
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);
+        }
+    })
+})
+
+// Add subject service
+app.post('/addSubject', (req, res) => {
+    console.log('req----------', req);
+    db.collection('subjectList').save(req.body, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('saved to database');
+        }
+    })
+})
+
+// Get all subject service
+app.get('/getAllSubjects', (req, res) => {
+    db.collection('subjectList').find().toArray((err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);
+        }
     })
 })
