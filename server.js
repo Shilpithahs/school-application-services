957b945a29f12f0727a7454489223d7966652875
Shilpitha HS <shilpitha.rb171@in.ad.dom>
2018-07-23 06:12:57 +0530

    Updated Changes

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-DATABASE_URL = 'mongodb://Shilpitha15:shil_mlab_15@ds243491.mlab.com:43491/m-lab-db';
+DATABASE_URL = 'mongodb://localhost:27017/codingDB';
 MongoClient.connect(DATABASE_URL, { useNewUrlParser: true }, (err, database) => {
     if(err) {
         console.log(err);
     } else {
-        db = database.db('m-lab-db') // your database name
+        db = database.db('codingDB') // your database name
         app.listen(3000, () => {
             console.log('listening on 3000')
         })
@@ -21,39 +21,47 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Content-Type", "application/json");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "access-control-allow-origin,content-type");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers, Access-Control-Allow-Origin, Content-Type");
+    res.header("Access-Control-Request-Headers","Access-Control-Allow-Headers, Access-Control-Allow-Origin, Content-Type");
     next();
 });
 
 // Add user service
-app.post('/user', (req, res) => {
-    db.collection('users').save(req.body, (err, result) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('user saved to database');
-    }
+app.post('/registerUser', (req, res) => {
+    db.collection('userList').save(req.body, (err, result) => {
+        if(err) {
+            console.log('error in saving to database');
+            return err;
+        } else {
+            console.log('user saved to database');
+            res.send(result.ops);
+            return result.ops;
+        }
     })
 })
 
 // Get user service
-app.get('/user', (req, res) => {
-    db.collection('users').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    console.log(result);
-    res.json(result);
+app.get('/getAllUsers', (req, res) => {
+    db.collection('userList').find().toArray((err, result) => {
+        if(err) {
+            return err;
+        } else {
+            res.send(result);
+            return result;
+        }
     })
 })
 
 // Add Student service
-app.post('/addSutudent', (req, res) => {
+app.post('/addStudent', (req, res) => {
     db.collection('studentList').save(req.body, (err, result) => {
         if(err) {
-            console.log(err);
+            return err;
         } else {
-            console.log('saved to database');
+            console.log('student saved to database');
+            res.send(result.ops);
+            return result.ops;
         }
     })
 })
@@ -64,20 +72,21 @@ app.get('/getAllStudents', (req, res) => {
         if(err) {
             console.log(err);
         } else {
-            console.log(result);
-            res.json(result);
+            res.send(result);
+            return result;
         }
     })
 })
 
 // Add subject service
 app.post('/addSubject', (req, res) => {
-    console.log('req----------', req);
     db.collection('subjectList').save(req.body, (err, result) => {
         if(err) {
-            console.log(err);
+            return err;
         } else {
-            console.log('saved to database');
+            console.log('subject saved to database');
+            res.send(result.ops);
+            return result.ops;
         }
     })
 })
@@ -88,8 +97,54 @@ app.get('/getAllSubjects', (req, res) => {
         if(err) {
             console.log(err);
         } else {
-            console.log(result);
-            res.json(result);
+            res.send(result);
+            return result;
+        }
+    })
+})
+
+app.delete('/deleteStudent', (req, res) => {
+    db.collection('studentList').findOneAndDelete(req.body, (err, result) => {
+        if(err) {
+            console.log('err-----', err);
+            return err;
+        } else {
+            console.log('student deleted from database');
+            res.send(result.ops);
+            return result.ops;
+        }
+    })
+})
+
+app.put('/updateStudent', (req, res) => {
+    db.collection('studentList').findOneAndUpdate({id: req.body.id}, 
+        {$set: 
+            {
+                email: req.body.email, 
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                phone: req.body.phone,
+                standard: req.body.standard,
+                subject: req.body.subject
+            }
+        }, (err, result) => {
+        if(err) {
+            return err;
+        } else {
+            console.log('student updated from database');
+            res.send(result.ops);
+            return result.ops;
+        }
+    })
+})
+
+app.get('/getStudentByEmailID', (req, res) => {
+    db.collection('studentList').find().toArray((err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+            return result;
         }
     })
 })
